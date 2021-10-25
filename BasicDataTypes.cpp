ca5a12dd21b1901344fdd6422a69cb65f2c46721
Sheikh Abdul Munim <sheikh00munim@gmail.com>
2021-10-25 15:08:29 +0530

    second

---
+++ BasicDataTypes.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    //cin>>a>>b>>c>>d;
+    scanf("%d\n", &a);
+    scanf("%ld\n", &b);
+    scanf("%c\n", &c);
+    scanf("%f\n", &d);
+    scanf("%lf\n", &e);
+    
+    printf("%d\n", a);
+    printf("%ld\n", b);
+    printf("%c\n", c);
+    printf("%f\n", d);
+    
+    /*printf("%lf\n", e);
+    printf("%lf\n", e);
+    printf("%lf\n", e);
+    printf("%lf\n", e);
+    
+    
+    cout<<a<<endl;
+    cout<<b<<endl;
+    cout<<c<<endl;
+    cout<<d<<endl;
+    //cout<<e;*/
+    printf("%lf\n", e);
+    
+    
+    // Complete the code.
+    return 0;
+}
