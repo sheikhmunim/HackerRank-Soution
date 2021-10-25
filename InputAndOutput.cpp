64b29818636ca3ced50e843ace4c35b391b47485
Sheikh Abdul Munim <sheikh00munim@gmail.com>
2021-10-25 12:20:44 +0530

    initial commit

---
+++ InputAndOutput.cpp
@@ -0,0 +1,16 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+
+int main() {
+    int i,m,n,l;
+    cin>>i>>m>>n;
+    l = m+n+i;
+    cout<<l;
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */   
+    return 0;
+}
